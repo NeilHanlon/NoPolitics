b1a56a82032291fd561810661dd788e4bbf86c93
Neil Hanlon <neil@neilhanlon.com>
2012-08-31 00:11:31 -0400

    Created initial list of regex to match, began testing matches...

--- assets/js/background.js
+++ assets/js/background.js
@@ -2,18 +2,18 @@
  * Handles requests sent by the content script.  Shows an infobar.
  */
 function onRequest(request, sender, sendResponse) {
-  // The number of matches is sent in the request - pass it to the
-  // infobar.
-  var url = "../../infobar.html#" + request.count;
+    // The number of matches is sent in the request - pass it to the
+    // infobar.
+    var url = "../../infobar.html#" + request.count;
+    
+    // Show the infobar on the tab where the request was sent.
+    chrome.experimental.infobars.show({
+        tabId: sender.tab.id,
+        path: url
+    });
 
-  // Show the infobar on the tab where the request was sent.
-  chrome.experimental.infobars.show({
-    tabId: sender.tab.id,
-    path: url
-  });
-
-  // Return nothing to let the connection be cleaned up.
-  sendResponse({});
+    // Return nothing to let the connection be cleaned up.
+    sendResponse({});
 };
 
 // Listen for the content script to send a message to the background page.
--- assets/js/regex.js
+++ assets/js/regex.js
@@ -1,8 +1,35 @@
-var regex = /sandwich/gi;
-matches = document.body.innerText.match(regex);
+var regex = [
+/romney/gi,
+/obama/gi,
+/mitt romney/gi,
+/barack obama/gi,
+/election\ 2012/gi,
+/us\ election/gi,
+/american\ president/gi,
+/liberal/gi,
+/conservative/gi,
+/democrat/gi,
+/republican/gi,
+/2012\ election/gi,
+/november\ election/gi
+];
+
+var newRegex = /^romney|obama|mitt(?=romney)|barack(?=obama)|election\ 2012|us\ election|liberal|conservative|republican|2012\ election|november\ election/gi;
+var matches = 0;
+var dbi = document.body.innerText;
+console.log(dbi.match(newRegex));
+console.log(dbi);
+for(var i= 0; i<regex.length;i++){
+    var match = dbi.match(regex[i]);
+    if(match != null){
+        matches += match.length;
+    }
+}
 if (matches) {
     var payload = {
-        count: matches.length    // Pass the number of matches back.
+        count: matches,    // Pass the number of matches back.
+        regex: regex,
+        dbi: dbi
     };
     chrome.extension.sendRequest(payload, function(response) {});
 }
